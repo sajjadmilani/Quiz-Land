2dd7ab05082148c9e83019be61518ec50e7c4726
Sajjad Milani <sajjad.milani@gmail.com>
2022-06-09 23:08:58 -0400

    Routes Correction

--- server/routes.js
+++ server/routes.js
@@ -8,18 +8,20 @@ const {
   getATrueFalse,
   getAQuestion } = require('./handlers/randomHandlers');
 
-
+//Qustions Endpoints
 router.get("/api/getRandom/MultiChoice", getAMultiChoice);
 router.get("/api/getRandom/TrueFalse", getATrueFalse);
 router.get("/api/getRandom/Question", getAQuestion);
 
-
+//Category Endpoints
 router.get("/api/getCategories", getCategories);
 
+//Quiz Endpoints
 router.post("/api/quiz", addQuiz);
 router.get("/api/quiz/:_id", getQuiz);
 router.get("/api/quizzes/:userId", getQuizzesByUser);
 
+//User Endpoints
 router.post("/api/user/authorize", authorizUser);
 router.get("/api/user/:_id", getUser);
 
